199106edfb1d21a979f71f05fd069d4d73e8f3c0
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-31 13:40:55 +0530

    add verbs for register and login route path

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -3,7 +3,7 @@ import { loginUser, registerUser } from "../controller/user.controller.js";
 
 const router = Router();
 
-router.post("/", registerUser);
-router.post("/", loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
