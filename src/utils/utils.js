cac24edc4cc2109aef779d3cdb24fc3190e6297d
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-31 13:38:47 +0530

    add generateToken in utils.js

---
+++ src/utils/utils.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (payload, res) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+};
